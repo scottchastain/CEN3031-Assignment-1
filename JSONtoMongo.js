89859ce0c370961e53cdb8245c4eae86683fd158
Scott <schas@ufl.edu>
2019-09-15 19:02:13 -0400

    Assignment Complete

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -1,28 +1,55 @@
 'use strict';
-/* 
-  Import modules/files you may need to correctly run the script. 
+/*
+  Import modules/files you may need to correctly run the script.
   Make sure to save your DB's uri in the config file, then import it with a require statement!
  */
 var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
     config = require('./config');
 
 /* Connect to your database using mongoose - remember to keep your key secret*/
 //see https://mongoosejs.com/docs/connections.html
 //See https://docs.atlas.mongodb.com/driver-connection/
 
-/* 
-  Instantiate a mongoose model for each listing object in the JSON file, 
-  and then save it to your Mongo database 
+mongoose.connect(config.db.uri);
+
+
+/*
+  Instantiate a mongoose model for each listing object in the JSON file,
+  and then save it to your Mongo database
   //see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
 
   Remember that we needed to read in a file like we did in Bootcamp Assignment #1.
  */
 
+ fs.readFile('listings.json', 'utf8', function(err, data) {
 
-/*  
-  Check to see if it works: Once you've written + run the script, check out your MongoLab database to ensure that 
-  it saved everything correctly. 
+     //Check for errors
+     if (err) throw err;
+     var listingData = JSON.parse(data);
+     listingData['entries'].forEach(function(element) {
+       var newListing =  Listing({
+         code: element.code,
+         name: element.name,
+         address: element.address
+       });
+       if (element.coordinates)
+        newListing.coordinates = {
+          latitude: element.coordinates.latitude,
+          longitude: element.coordinates.longitude
+        };
+
+       newListing.save(function(err) {
+         if (err) throw err;
+         console.log(newListing.code)
+       })
+     });
+ });
+
+
+/*
+  Check to see if it works: Once you've written + run the script, check out your MongoLab database to ensure that
+  it saved everything correctly.
  */
--- queries.js
+++ queries.js
@@ -1,39 +1,70 @@
 /* Add all the required libraries*/
-
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
+    config = require('./config');
 /* Connect to your database using mongoose - remember to keep your key secret*/
+
+mongoose.connect(config.db.uri);
 
 /* Fill out these functions using Mongoose queries*/
 //Check out - https://mongoosejs.com/docs/queries.html
 
 var findLibraryWest = function() {
-  /* 
+  /*
     Find the document that contains data corresponding to Library West,
-    then log it to the console. 
+    then log it to the console.
    */
+   Listing.find({name: "Library West" }, function(err, result) {
+      if (err) throw err;
+
+      console.log(result);
+   });
 };
+
 var removeCable = function() {
   /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
+    and remove this listing from your database and log the document to the console.
    */
+   Listing.findOneAndRemove({code: "CABL" }, function(err, result) {
+      if (err) throw err;
+      if (result)
+        console.log(result);
+      else {
+        console.log("Not Found");
+      }
+   });
+
 };
 var updatePhelpsLab = function() {
   /*
-    Phelps Lab address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
-    
+    Phelps Lab address is incorrect. Find the listing, update it, and then
+    log the updated document to the console.
+
     Correct Address: 1953 Museum Rd, Gainesville, FL 32603
 
    */
+
+   Listing.findOneAndUpdate({name: "Phelps Laboratory" }, {address:
+     "1953 Museum Rd, Gainesville, FL 32603"}, {new: true}, function(err, result) {
+      if (err) throw err;
+      console.log(result);
+   });
 };
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
+  /*
+    Retrieve all listings in the database, and log them to the console.
    */
+
+  Listing.find({}, function(err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
 };
 
 findLibraryWest();
 removeCable();
-updatePhelpsMemorial();
+updatePhelpsLab();
 retrieveAllListings();
